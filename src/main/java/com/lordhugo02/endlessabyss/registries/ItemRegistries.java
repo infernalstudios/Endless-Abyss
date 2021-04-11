5b6c1b68e9a64414cfb493da4676fecb3adc6f8d
LordHugo02 <75981726+LordHugo02@users.noreply.github.com>
2021-04-11 13:41:18 +0200

    Initial commit

---
+++ src/main/java/com/lordhugo02/endlessabyss/registries/ItemRegistries.java
@@ -0,0 +1,18 @@
+package com.lordhugo02.endlessabyss.registries;
+
+import com.lordhugo02.endlessabyss.EndlessAbyss;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ItemRegistries
+{
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, EndlessAbyss.modid);
+
+    public static final RegistryObject<Item> test = ITEMS.register("hellstone", () -> new Item (new Item.Properties().group(ItemGroup.MISC)));
+
+
+}
